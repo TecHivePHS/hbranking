d90954eaf100da02f3252d430f3f54c1ee2f2aa0
TecHive '25 <smcs2025x.techive@gmail.com>
2023-08-05 10:30:17 -0400

    Update sorting.py

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,10 +1,15 @@
 class Scout:
   team = 4099
 
-  def __init__(self, name):
+  def __init__(self, name, subteam):
     self.name = name
+    self.subteam = subteam
 
-scout_one = new Scout("John")
+scout_one = new Scout("John", "strategy")
+scout_two = new Scout("Nhoj", "business")
 
-print(scout_one.team)
+# Instance access
+print(scout_one.team) 
+print(scout_two.team)
+# Class access
 print(Scout.team)

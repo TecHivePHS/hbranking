8d0a85c72267594945b0f5e5de0ec886a3a31c49
TecHive '25 <smcs2025x.techive@gmail.com>
2023-08-08 16:47:42 -0400

    Update sorting.py

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,31 +1,7 @@
-from abc import ABC, abstractmethod
+numbers = [i for i in range(20)]
+squares = {i: i**2 for i in range(20)}
+odds = (i              # Output expression
+        for i          # Input variable
+        in range(20)   # Input sequence
+        if i % 2 == 1) # Conditional
 
-class Shape(ABC):
-  def __init__(self):
-    pass
-  @abstractmethod
-  def info(self):
-    pass
-
-class Quadrilateral(Shape):
-  def __init__(self, angles):
-    self.sides = 4
-    self.angles = angles
-
-class Scout:
-  team = 4099
-  def __init__(self, name, subteam):
-    self.name = name
-    self.subteam = subteam
-  def display():
-    print(f"{self.name} is a scout.")
-
-class PitScout(Scout):
-  def __init__(self, name, subteam, group):
-    super().__init__(name, subteam)
-    self.group = group
-  def display(): # Overriden
-    print(f"{self.name} is a pit scout.")
-
-scout_one = new Scout("John", "strategy")
-scout_two = new Scout("Nhoj", "business")
